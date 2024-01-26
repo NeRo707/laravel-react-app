824e7df03beb059d2faec9612b226e8c2a05bd7a
NeRo707 <tazo.gorgadze.02@gmail.com>
2024-01-26 23:40:26 +0400

    first commit

---
+++ react/src/router.jsx
@@ -0,0 +1,58 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/Signup";
+import Users from "./pages/Users";
+import NotFound from "./pages/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./pages/Dashboard.jsx";
+import UserForm from "./pages/UserForm.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/users" />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+      {
+        path: "/users/:id",
+        element: <UserForm key="userUpdate" />,
+      },
+      {
+        path: "/users/new",
+        element: <UserForm key="userCreate" />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
